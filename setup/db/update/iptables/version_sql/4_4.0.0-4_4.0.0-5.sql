68196072d4ce8370786629e54768ffd0432a7c50
Nicolas Gudino <asternic@gmail.com>
2021-06-08 07:34:56 -0400

    Add filter rules to allow webrtc and jabber over secure websocket

--- setup/db/update/iptables/version_sql/4_4.0.0-4_4.0.0-5.sql
+++ setup/db/update/iptables/version_sql/4_4.0.0-4_4.0.0-5.sql
@@ -1,2 +1,25 @@
 INSERT INTO port(name,protocol,details,comment) VALUES ('WEBRTC', 'TCP', '8088:8089', '');
+
+UPDATE filter SET rule_order = rule_order + 1 WHERE rule_order >= (
+    SELECT rule_order FROM filter WHERE dport = (SELECT id FROM port WHERE name = 'SIP')
+);
+INSERT INTO filter (traffic,eth_in,eth_out,ip_source,ip_destiny,protocol,sport,dport,icmp_type,number_ip,target,rule_order,activated)
+VALUES ('INPUT','ANY','','0.0.0.0/0','0.0.0.0/0','TCP','ANY',
+    (SELECT id FROM port WHERE name = 'WEBRTC'),
+    '','','ACCEPT',
+    (SELECT rule_order FROM filter WHERE dport = (SELECT id FROM port WHERE name = 'SIP')) - 1,
+    1);
+
+
 INSERT INTO port(name,protocol,details,comment) VALUES ('XMPP/WSS', 'TCP', '5281', '');
+UPDATE filter SET rule_order = rule_order + 1 WHERE rule_order >= (
+    SELECT rule_order FROM filter WHERE dport = (SELECT id FROM port WHERE name = 'SIP')
+);
+INSERT INTO filter (traffic,eth_in,eth_out,ip_source,ip_destiny,protocol,sport,dport,icmp_type,number_ip,target,rule_order,activated)
+VALUES ('INPUT','ANY','','0.0.0.0/0','0.0.0.0/0','TCP','ANY',
+    (SELECT id FROM port WHERE name = 'XMPP/WSS'),
+    '','','ACCEPT',
+    (SELECT rule_order FROM filter WHERE dport = (SELECT id FROM port WHERE name = 'SIP')) - 1,
+    1);
+
+UPDATE tmp_execute SET exec_in_sys = 0;
